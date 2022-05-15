718470e269ffd1071a0383e8a7f20567299c285e
Karol Kot <karol.kot@aerdata.com>
2022-05-15 11:23:01 +0200

    Problem 20 solution

---
+++ problems/Problem20/problem.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+def multiply_last_items(numbers: List[int]) -> List[int]:
+    for index in range(0, len(numbers)):
+        end_index = len(numbers) - 1 - index
+        if index >= end_index:
+            return [number for number in numbers if number > 0]
+
+        numbers[index] = numbers[index] * numbers[end_index]
+        numbers[end_index] = 0
+
+
+def get_factorial(number: int):
+    numbers = [number for number in range(1, number + 1)]
+
+    while len(numbers) > 1:
+        numbers = multiply_last_items(numbers)
+
+    return numbers[0]
+
+
+def get_digits_sum_for_factorial(number: int):
+    factorial_string = str(get_factorial(number))
+
+    digits_sum = 0
+    for char in factorial_string:
+        digits_sum += int(char)
+
+    return digits_sum
---
+++ tests/Problem20/test_problem.py
@@ -0,0 +1,25 @@
+import unittest
+from datetime import datetime
+
+from problems.Problem19.problem import is_leap_year, get_all_sundays_between_days, get_first_sunday_date_from, \
+    get_all_sundays_between_days_which_are_first_day_of_month
+from problems.Problem20.problem import get_factorial, get_digits_sum_for_factorial
+
+
+class TestsProblem20(unittest.TestCase):
+
+    def test_get_factorial_return_6_for_number_3(self):
+        result = get_factorial(3)
+        self.assertEqual(6, result)
+
+    def test_get_factorial_return_6_for_number_3(self):
+        result = get_factorial(100)
+        self.assertEqual(93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000, result)
+
+    def test_get_digits_sum_for_factorial(self):
+        result = get_digits_sum_for_factorial(100)
+        self.assertEqual(648, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
