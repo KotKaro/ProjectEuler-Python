679c6a6f70a2e538fae02dbce4a0312c24a8a53c
Karol Kot <karol.kot@aerdata.com>
2022-05-22 11:04:24 +0200

    optimized

--- problems/Problem788/problem.py
+++ problems/Problem788/problem.py
@@ -16,36 +16,13 @@ def count_of_dominating_numbers_in_power(power: int):
     minimal_count_of_same_numbers = int((total_digits_count / 2)) + 1
 
     total = 0
-    total_not_zero = 0
-    total_zero = 0
 
     for index in range(minimal_count_of_same_numbers, total_digits_count + 1):
-        # 1 - 9
         total_not_zero_remaining = total_digits_count - index
-        to_multiply = 1 if total_not_zero_remaining == 0 else 8 ** total_not_zero_remaining
-        total_not_zero += 9 * get_placement_combinations(total_digits_count, index) * to_multiply
+        to_multiply = 1 if total_not_zero_remaining == 0 else 9 ** total_not_zero_remaining
+        total += 9 * get_placement_combinations(total_digits_count, index) * to_multiply
 
-        if total_digits_count - 1 < index:
-            continue
-
-        # count of zeros >= minimal_count_of_same_numbers
-        zero_remaining_number = total_digits_count - index - 1
-        to_multiply = 1 if zero_remaining_number <= 0 else 9 ** zero_remaining_number
-        total_zero += 9 * get_placement_combinations(total_digits_count - 1, index) * to_multiply
-
-        # count of zeros < minimal_count_of_same_numbers
-        for zeros_count in range(1, minimal_count_of_same_numbers):
-            if zeros_count + index > total_digits_count:
-                continue
-
-            zeros_placements = get_placement_combinations(total_digits_count - 1, zeros_count)
-            non_zeros_placements = get_placement_combinations(total_digits_count - zeros_count,
-                                                              index)
-            other_numbers = total_digits_count - (index + zeros_count)
-            to_multiply = 1 if other_numbers <= 0 else 8 ** other_numbers
-            total += 9 * non_zeros_placements * zeros_placements * to_multiply
-
-    return total + total_zero + total_not_zero
+    return total
 
 
 def count_of_dominating_numbers_to_power(power: int):
@@ -57,15 +34,3 @@ def count_of_dominating_numbers_to_power(power: int):
         all_powers.append(count_of_dominating_numbers_in_power(power))
         print("--- %s seconds ---" % (time.time() - start_time))
     return sum(all_powers)
-
-
-if __name__ == '__main__':
-    start_time = time.time()
-
-    # wzor n!(n-k)!
-
-    n = 2022
-    comb = (int(n / 2) + 1) ** 10 * ((int(n / 2) - 1) ** 10)
-    result = comb % 1_000_000_007;
-
-    print("--- %s seconds ---" % (time.time() - start_time))
