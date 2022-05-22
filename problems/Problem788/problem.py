7ff9f1de8983308a406c2583e08f126b6c42abe7
Karol Kot <karol.kot@aerdata.com>
2022-05-22 08:51:22 +0200

    Working algorithm - to be optimized

--- problems/Problem788/problem.py
+++ problems/Problem788/problem.py
@@ -2,21 +2,6 @@ import math
 import time
 
 
-def is_dominating_number(number: int):
-    string_number = str(number)
-    string_number_half_length = len(string_number) / 2
-    keys_dictionary = {}
-    for char in string_number:
-        if char in keys_dictionary:
-            keys_dictionary[char] = keys_dictionary[char] + 1
-        else:
-            keys_dictionary[char] = 1
-        if keys_dictionary[char] > string_number_half_length:
-            return True
-
-    return False
-
-
 def get_placement_combinations(available_places: int, places_to_fill: int) -> int:
     # possible placement combinations
     # n - possible ways
@@ -30,7 +15,6 @@ def count_of_dominating_numbers_in_power(power: int):
     total = 0
     total_digits_count = power
     minimal_count_of_same_numbers = int((total_digits_count / 2)) + 1
-    remaining_number = total_digits_count - minimal_count_of_same_numbers
 
     total_not_zero = 0
     for index in range(minimal_count_of_same_numbers, total_digits_count + 1):
@@ -50,35 +34,24 @@ def count_of_dominating_numbers_in_power(power: int):
                 continue
 
             zeros_placements = get_placement_combinations(total_digits_count - 1, count_of_zeros)
-            non_zeros_placements = get_placement_combinations(total_digits_count - count_of_zeros, minimal_count_of_same)
+            non_zeros_placements = get_placement_combinations(total_digits_count - count_of_zeros,
+                                                              minimal_count_of_same)
             other_numbers = total_digits_count - (minimal_count_of_same + count_of_zeros)
             to_multiply = 1 if other_numbers <= 0 else 8 ** other_numbers
             total += 9 * non_zeros_placements * zeros_placements * to_multiply
-
 
     return total + total_zero + total_not_zero
 
 
 def count_of_dominating_numbers_to_power(power: int):
     all_powers = []
-    for power in range(1, power + 1):
+    powers = list(reversed(range(1, power + 1)))
+
+    for power in powers:
         print("Calculating for: {0}".format(power))
         all_powers.append(count_of_dominating_numbers_in_power(power))
 
     return sum(all_powers)
-
-
-def count_of_dominating_numbers_to_power_slow(power: int, digit_to_replace: int = None, times: int = None):
-    total = 0
-    for number in range(10**(power - 1), 10 ** power):
-        if is_dominating_number(number):
-            if digit_to_replace is not None and times is not None:
-                if len(str(number)) - len(str(number).replace(str(digit_to_replace), '')) == times:
-                    print(number)
-
-            total += 1
-
-    return total
 
 
 if __name__ == '__main__':
--- tests/Problem788/test_problem.py
+++ tests/Problem788/test_problem.py
@@ -1,28 +1,21 @@
 import unittest
 
-from problems.Problem788.problem import is_dominating_number, count_of_dominating_numbers_in_power, \
-    count_of_dominating_numbers_to_power, count_of_dominating_numbers_to_power_slow
+from problems.Problem788.problem import count_of_dominating_numbers_in_power, \
+    count_of_dominating_numbers_to_power
 
 
 class TestsProblem24(unittest.TestCase):
-
-    def test_is_dominating_number_for_2022_returns_true(self):
-        result = is_dominating_number(2022)
-        self.assertEqual(True, result)
-
-    def test_is_dominating_number_for_1999_returns_true(self):
-        result = is_dominating_number(1999)
-        self.assertEqual(True, result)
-
-    def test_is_dominating_number_for_1337_returns_false(self):
-        result = is_dominating_number(1337)
-        self.assertEqual(False, result)
 
     def test_count_of_dominating_numbers_in_power_should_return_expected_count(self):
         parameter_result = [
             [3, 252],
             [4, 333],
-            [5, 7704]
+            [5, 7704],
+            [6, 11430],
+            [7, 245520],
+            [8, 388485],
+            [9, 8018280],
+            [10, 13221234]
         ]
         for number, expected_result in parameter_result:
             with self.subTest('{0} -> {1}'.format(number, expected_result)):
@@ -32,33 +25,9 @@ class TestsProblem24(unittest.TestCase):
                 # assert
                 self.assertEqual(result, expected_result)
 
-    def test_count_of_dominating_numbers_to_power_slow_should_return_expected_count(self):
-        parameter_result = [
-            [3, 252],
-            [4, 333],
-            [5, 7704],
-            # without zero should be 6129
-            # with zero should be 1575
-            [10, 21893256]
-        ]
-        for number, expected_result in parameter_result:
-            with self.subTest('{0} -> {1}'.format(number, expected_result)):
-                # act
-                result = count_of_dominating_numbers_to_power_slow(number)
-
-                # assert
-                self.assertEqual(result, expected_result)
-
     def test_count_of_dominating_numbers_to_power_should_return_expected_count_1(self):
         # act
         result = count_of_dominating_numbers_in_power(5)
-
-        # assert
-        self.assertEqual(result, 7704)
-
-    def test_count_of_dominating_numbers_to_power_slow_should_return_expected_count_1(self):
-        # act
-        result = count_of_dominating_numbers_to_power_slow(5, 0, 1)
 
         # assert
         self.assertEqual(result, 7704)
