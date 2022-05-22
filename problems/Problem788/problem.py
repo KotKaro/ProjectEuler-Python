be5709a7c5520918b0232694b79b928c942d6656
Karol Kot <karol.kot@aerdata.com>
2022-05-22 09:39:54 +0200

    update

--- problems/Problem788/problem.py
+++ problems/Problem788/problem.py
@@ -7,23 +7,26 @@ def get_placement_combinations(available_places: int, places_to_fill: int) -> in
     # n - possible ways
     # p - places to fill
     # n!/p!(n-p)!
-    return math.factorial(available_places) / (math.factorial(places_to_fill) * math.factorial(
+    return math.factorial(available_places) // (math.factorial(places_to_fill) * math.factorial(
         available_places - places_to_fill))
 
 
 def count_of_dominating_numbers_in_power(power: int):
-    total = 0
     total_digits_count = power
     minimal_count_of_same_numbers = int((total_digits_count / 2)) + 1
 
+    total = 0
     total_not_zero = 0
+    total_zero = 0
+
     for index in range(minimal_count_of_same_numbers, total_digits_count + 1):
         total_not_zero_remaining = total_digits_count - index
         to_multiply = 1 if total_not_zero_remaining == 0 else 8 ** total_not_zero_remaining
         total_not_zero += 9 * get_placement_combinations(total_digits_count, index) * to_multiply
 
-    total_zero = 0
-    for index in range(minimal_count_of_same_numbers, total_digits_count):
+        if total_digits_count - 1 < index:
+            continue
+
         zero_remaining_number = total_digits_count - index - 1
         to_multiply = 1 if zero_remaining_number <= 0 else 9 ** zero_remaining_number
         total_zero += 9 * get_placement_combinations(total_digits_count - 1, index) * to_multiply
